cb9442902beba51bf53a6a033be96bac32017c8a
Kyle Onda <kso8@cdss-6342.lan>
2019-10-22 18:21:09 -0400

    Reorganize

--- shinyApp/Glossary/tm_test.R
+++ shinyApp/Glossary/tm_test.R
@@ -7,8 +7,12 @@ library(arules)
 
 # convert to format for text mining: term label- vocabulary concatenation/ definition
 
-#read as corpus
-my.corpus <- Corpus(DirSource("corpus/r-corpus"))
+#read as 
+df<-distinct(data[c("uri","Definition","Term","Vocabulary")],uri,.keep_all=T)
+colnames(df)[1]<-"doc_id"
+colnames(df)[2]<-"text"
+
+my.corpus <- Corpus(DataframeSource(df))
 
 #remove punctuation and stopwords and lemmatize (remove plurals and hyphenation and such)
 my.corpus <- tm_map(my.corpus, removePunctuation)
@@ -17,7 +21,16 @@ my.corpus <- tm_map(my.corpus, removeWords, stopwords("english"))
 my.corpus <- tm_map(my.corpus, stemDocument)
 
 #Process to matrix
-my.tdm <- TermDocumentMatrix(tm::inspect(my.corpus))
+tdm <- TermDocumentMatrix(tm::inspect(my.corpus))
+
+
+library(slam)
+cosine_dist_mat <- 1 - crossprod_simple_triplet_matrix(tdm)/(sqrt(col_sums(tdm^2) %*% t(col_sums(tdm^2))))
+
+
+tdm <- as.matrix(tdm)
+require(proxy)
+cosine_dist_mat <- as.matrix(dist(t(tdm), method = "cosine"))
 
 #scale word counts
 
