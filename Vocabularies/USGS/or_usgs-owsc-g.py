5780505979e1ca7adfaeed32decdb0632c44eede
JBrewer <jbrewer256@gmail.com>
2020-02-22 11:56:44 -0700

    USGS: made progress in importing this 400+ word dictionary

--- Vocabularies/USGS/or_usgs-owsc-g.py
+++ Vocabularies/USGS/or_usgs-owsc-g.py
@@ -10,9 +10,10 @@ import requests #to pull info from an html site
 from bs4 import BeautifulSoup #to parse and sort html information
 import pandas as pd # parsing data and creating dataframes
 import os
+import numpy as np
 import openpyxl
 
-workingDir="/Users/joseph/Desktop/WSWC/IoW/Vocabularies/ID/"
+workingDir="/Users/augustus/Desktop/WSWC/IoW/Vocabularies/USGS/"
 os.chdir(workingDir)
 
 
@@ -20,7 +21,7 @@ os.chdir(workingDir)
 ############################################################################
 result = requests.get("https://or.water.usgs.gov/projs_dir/willgw/glossary.html")  #linking to website and creating source.
 html = result.content  # store the page content
-soup = BeautifulSoup(html, 'html.parser')  #create a beautifulSoup obect based on the source variable.
+soup = BeautifulSoup(html, 'lxml')  #create a beautifulSoup obect based on the source variable.
 
 
 #Storage Variables
@@ -35,13 +36,12 @@ lst3 = []
 if result.status_code == 200:  # a valuve of 200 indicates yes, a 403 forbidden means no.
     print("Success. Website is accessible.")
 
-    neededclass = soup.find(id = "mainbody")  # ID-WRB specific. Finding all 'tr' tag items within tr tags to seperate term from
-                                              # definition.
+    neededclass = soup.find(id = "mainbody")
+
+
     for p in neededclass.find_all('p'):
-            lst1.append(p.text)
+        lst1.append(p.text)
 
-
-#    data = {'Terms': lst1, 'Definitions': lst2}
     glossary = pd.DataFrame(lst1)
 
 
@@ -50,8 +50,33 @@ if result.status_code == 200:  # a valuve of 200 indicates yes, a 403 forbidden 
     print("Cleaning text.")
     # ID DEQ Specific.
 
+    # drop references from end of dataframe
+    ref = list(range(436, 472))
+    glossary.drop(ref, inplace=True)
+
+    for i, row in glossary.iterrows():
+        x = len(row[0])
+        y = row[0]
+        print(i, x)
+        if x <= 4:
+            glossary.drop(i, inplace = True)
+        elif "|" in y:
+            glossary.drop(i, inplace =True)
+
+
+
+        print('done')
+
+    glossary = pd.Series(glossary)
+    glossary = glossary.str.strip()
+
+
+
     # remove first row
-    glossary.drop([0], inplace=True)
+    glossary.drop([0], inplace=True)  # remove first row
+
+
+
     glossary.set_index('Terms', inplace=True)
 
 
