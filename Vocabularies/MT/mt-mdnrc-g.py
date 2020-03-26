c08e597d40c4c442548481bf7efa56ba5a77ba28
JBrewer <jbrewer256@gmail.com>
2020-03-25 18:36:12 -0600

    MT:  Finished DNRC 2015 State Water Plan

--- Vocabularies/MT/mt-mdnrc-g.py
+++ Vocabularies/MT/mt-mdnrc-g.py
@@ -1,36 +1,65 @@
-import requests
-import PyPDF2
-import io
+#Date Created: 03/25/2020
+#Purpose: To extract text from html file (website) and export to xlsx file.
+#Notes: # MT DNRC State Water Plan glossary.
+
+
+#Needed Modules
+############################################################################
+from docx import *
+import os
 import pandas as pd
 
-
-url = "http://dnrc.mt.gov/divisions/water/management/docs/state-water-plan/2015_mt_water_plan.pdf"
-file = '/Users/joseph/Desktop/2016_water_commissioner_glossary.pdf'
-
-result = requests.get(url)
-html = io.BytesIO(result.content)
-
-contents = ""
+workingDir="/Users/joseph/Desktop/WSWC/IoW/Vocabularies/MT/"
+os.chdir(workingDir)
 
 
-reader = PyPDF2.PdfFileReader(html)
-
-count = 81
-pageObj = reader.getPage(count)
-contents += pageObj.extractText()
-num_pages = reader.numPages
-contents = ""
-
-while count < num_pages + 1:
-    pageObj = reader.getPage(count)
-    count += 1
-    contents += pageObj.extractText()
-    print('success')
+#Retrieving Document
+############################################################################
+document = Document('2015-mt-water-plan.docx')
 
 
+#Storage Variables
+############################################################################
+# Create empty dataframes
+data = pd.DataFrame(columns=['glossary'])
+glossary = pd.DataFrame(columns=['Term', 'Definition'])
 
-df = pd.DataFrame(contents)
+
+#Gathering Data
+###########################################################################
+# Iterate over paragraghs.  Paragraphs seperated by columns
+for para in document.paragraphs:
+    data = data.append({'glossary':para.text}, ignore_index=True) # Append text to data DataFrame
+
+data.drop(72, inplace=True)
+
+# Iterate over rows in data DataFrame and split on ' – '
+for i, row in data.iterrows():
+    x = row['glossary'].split(' – ')
+    glossary = glossary.append({'Term':x[0], 'Definition':x[1]}, ignore_index=True) # Append split values to glossary dataframe
+                                                                                    # with positional arguments
+
+# Cleaning Text
+############################################################################
+# Strip leading and trailing whitespace
+stripped = pd.Series(glossary['Term'].values)  # Convert Keyword column to Pandas Series
+stripped = stripped.str.strip()  # Use Series.str.strip() to strip leading and trailing whitespace
+glossary = glossary.assign(Term=stripped)
+
+stripped = pd.Series(glossary['Definition'].values)
+stripped = stripped.str.strip()
+glossary = glossary.assign(Definition=stripped)
+
+
+# Exporting Text to xlsx file.
+############################################################################
+print("Exporting data to xlsx.")
+
+#export DataFrame to xlsx
+
+glossary.to_excel('mt-dnrc-g.xlsx')
+
+
 
 print('done')
 
-#Œ
