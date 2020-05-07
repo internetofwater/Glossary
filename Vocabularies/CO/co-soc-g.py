72c1b6966ad66f32f96dc4c500ebaabb8b2d199c
JBrewer <jbrewer256@gmail.com>
2020-05-07 17:09:15 -0600

    CO - Created and finished classifying two sets of terms for the Colorado Foundation for Water Education and the State of Colorado. USGS - Finished Oregon USGS glossary.

--- Vocabularies/CO/co-soc-g.py
+++ Vocabularies/CO/co-soc-g.py
@@ -1,8 +1,8 @@
-# Title: Colorado - Oregon USGS - Oregon Water Science Center - Glossary of Hydrologic Terms
+# Title: Colorado - State of Colorado - Glossary of Water Terms
 # Created by: Joseph Brewer, WSWC
 # Date Created: 05/6/2020
 # Purpose: To extract text from html file (website) and export to xlsx file.
-# Notes:
+# Notes: A text file stored somewhere in the bowel of colorado.gov
 
 
 #Needed Modules
@@ -13,13 +13,13 @@ import pandas as pd
 import openpyxl
 from openpyxl.utils.dataframe import dataframe_to_rows
 
-workingDir="/Users/augustus/Desktop/WSWC/IoW/Vocabularies/USGS/"
+workingDir="/Users/augustus/Desktop/WSWC/IoW/Vocabularies/CO/"
 os.chdir(workingDir)
 
 
 #Retrieving Document
 ############################################################################
-document = Document('or-or_usgs-g.docx')
+document = Document('co-soc-g.docx')
 
 
 #Storage Variables
@@ -40,18 +40,15 @@ for para in document.paragraphs:
 # Iterate over rows in data DataFrame and split on ' – '
 for i, row in data.iterrows():
     x = row['glossary'].split(' – ')
-    if len(x) == 2:
-        glossary = glossary.append({'Term':x[0], 'Definition':x[1]}, ignore_index=True) # Append split values to glossary dataframe
-                                                                                        # with positional arguments
-    else:
-        print(x)
+    glossary = glossary.append({'Term':x[0], 'Definition':x[1]}, ignore_index=True) # Append split values to glossary dataframe
+                                                                                    # with positional arguments
 
 # Cleaning Text
 ############################################################################
 # Strip leading and trailing whitespace
 stripped = pd.Series(glossary['Term'].values)  # Convert Keyword column to Pandas Series
 stripped = stripped.str.strip() # Use Series.str.strip() to strip leading/trailing whitespace
-#stripped = stripped.str.capitalize() # Use Series.str.capitalize to capitalize first character of string
+stripped = stripped.str.capitalize() # Use Series.str.capitalize to capitalize first character of string
 glossary = glossary.assign(Term=stripped)
 
 stripped = pd.Series(glossary['Definition'].values)
@@ -66,7 +63,7 @@ print("Exporting data to xlsx.")
 
 
 #export DataFrame to xlsx
-glossary.to_excel('or-or_usgs-g.xlsx')  #ToDo: un-comment to run again
+glossary.to_excel('co-soc-g.xlsx')  #ToDo: un-comment to run again
 
 
 print('done')
